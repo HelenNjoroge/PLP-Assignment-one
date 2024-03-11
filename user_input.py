2fcc2c7303b72a9bbb3bc4950e11bcfcfa5d710a
HelenWairimu <helenanjoro@gmail.com>
2024-03-11 15:59:25 +0300

    my first commit

---
+++ user_input.py
@@ -0,0 +1,11 @@
+name= input("What is your name?: ")
+print(name)
+
+age = input("How old are you?: ")
+print(age)
+
+location = input("Where do you live?: ")
+print(location)
+
+x=("Hello "+name + " you are "+age + "years today and you live in "+location)
+print(x)
